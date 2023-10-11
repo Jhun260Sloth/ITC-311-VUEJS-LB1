cc1ceb728e3f4fec6f39a633286d935e0f4409ed
Jhunz <99332717+Jhun260Sloth@users.noreply.github.com>
2023-10-11 22:14:32 +0800

    Jhunnel Part

--- src/views/ManagementView.vue
+++ src/views/ManagementView.vue
@@ -1,14 +1,19 @@
-<style>
-
-
-</style>
-
-
 <template>
-
-
-</template>
-
-<script>
-
-</script>
+    <Manage />
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import Manage from '../components/Manage.vue';
+  
+  export default defineComponent({
+    name: 'ManagementView',
+  
+    components: {
+        Manage,
+    },
+  });
+  </script>
+  
